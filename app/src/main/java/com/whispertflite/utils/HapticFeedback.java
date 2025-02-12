f8f9f0ed9bba0258322edf4aff321cdfad095868
woheller69 <wolfgang.heller@web.de>
2025-02-12 09:30:46 +0100

    make haptic feedback depend on system settings

--- app/src/main/java/com/whispertflite/utils/HapticFeedback.java
+++ app/src/main/java/com/whispertflite/utils/HapticFeedback.java
@@ -6,16 +6,29 @@ import android.content.Context;
 import android.os.Build;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
+import android.os.VibratorManager;
+import android.provider.Settings;
 
 public class HapticFeedback {
 
     public static void vibrate(Context context){
-        Vibrator vibrator = (Vibrator) context.getSystemService(VIBRATOR_SERVICE);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            vibrator.vibrate(VibrationEffect.createPredefined(VibrationEffect.EFFECT_HEAVY_CLICK));
+        if (hapticEnabled(context)){
+            Vibrator vibrator = (Vibrator) context.getSystemService(VIBRATOR_SERVICE);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                vibrator.vibrate(VibrationEffect.createPredefined(VibrationEffect.EFFECT_HEAVY_CLICK));
+            } else {
+                VibrationEffect vibrationEffect = VibrationEffect.createOneShot(10, 255);
+                vibrator.vibrate(vibrationEffect);
+            }
+        }
+    }
+
+    private static boolean hapticEnabled(Context context){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            VibratorManager vibratorManager = (VibratorManager) context.getSystemService(Context.VIBRATOR_MANAGER_SERVICE);
+            return vibratorManager.getDefaultVibrator().hasVibrator();
         } else {
-            VibrationEffect vibrationEffect = VibrationEffect.createOneShot(10, 255);
-            vibrator.vibrate(vibrationEffect);
+            return Settings.System.getInt(context.getContentResolver(), Settings.System.HAPTIC_FEEDBACK_ENABLED, 0) == 1;
         }
     }
 }
