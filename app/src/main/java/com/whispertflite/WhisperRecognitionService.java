3217784bf9f3ec844cac7a646432485df98fd954
woheller69 <woheller69@t-online.de>
2025-03-02 06:33:23 +0100

    add simple RecognitionService for a 5 seconds interval after start #53

---
+++ app/src/main/java/com/whispertflite/WhisperRecognitionService.java
@@ -0,0 +1,155 @@
+package com.whispertflite;
+
+import static android.speech.SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS;
+import static com.whispertflite.MainActivity.ENGLISH_ONLY_MODEL_EXTENSION;
+import static com.whispertflite.MainActivity.ENGLISH_ONLY_VOCAB_FILE;
+import static com.whispertflite.MainActivity.MULTILINGUAL_VOCAB_FILE;
+import static com.whispertflite.MainActivity.MULTI_LINGUAL_MODEL_SLOW;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.RemoteException;
+import android.speech.RecognitionService;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.util.Log;
+
+import androidx.core.content.ContextCompat;
+
+import com.whispertflite.asr.Recorder;
+import com.whispertflite.asr.Whisper;
+import com.whispertflite.asr.WhisperResult;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class WhisperRecognitionService extends RecognitionService {
+    private static final String TAG = "WhisperRecognitionService";
+    private Recorder mRecorder = null;
+    private Whisper mWhisper = null;
+    private File sdcardDataFolder = null;
+    private File selectedTfliteFile = null;
+    private boolean recognitionCancelled = false;
+
+    @Override
+    protected void onStartListening(Intent recognizerIntent, Callback callback) {
+        Log.d("WhisperRecognition","StartListening in " + recognizerIntent.getStringExtra(RecognizerIntent.EXTRA_LANGUAGE));
+
+        checkRecordPermission(callback);
+
+        sdcardDataFolder = this.getExternalFilesDir(null);
+        selectedTfliteFile = new File(sdcardDataFolder, MULTI_LINGUAL_MODEL_SLOW);
+
+        initModel(selectedTfliteFile, callback);
+
+        mRecorder = new Recorder(this);
+        mRecorder.setListener(message -> {
+        if (message.equals(Recorder.MSG_RECORDING_DONE)) {
+                startTranscription();
+        }
+        });
+
+        if (!mWhisper.isInProgress()) {
+            startRecording();
+            try {
+                callback.beginningOfSpeech();
+            } catch (RemoteException e) {
+                throw new RuntimeException(e);
+            }
+            Handler handler = new Handler(Looper.getMainLooper());
+            handler.postDelayed(this::stopRecording, 5000);
+        }
+
+    }
+
+    private void stopRecording() {
+        if (mRecorder != null && mRecorder.isInProgress()) {
+            mRecorder.stop();
+        }
+    }
+
+    @Override
+    protected void onCancel(Callback callback) {
+        Log.d("WhisperRecognition","cancel");
+        stopRecording();
+        recognitionCancelled = true;
+    }
+
+    @Override
+    protected void onStopListening(Callback callback) {
+        Log.d("WhisperRecognition","StopListening");
+        stopRecording();
+    }
+
+    // Model initialization
+    private void initModel(File modelFile, Callback callback) {
+        boolean isMultilingualModel = !(modelFile.getName().endsWith(ENGLISH_ONLY_MODEL_EXTENSION));
+        String vocabFileName = isMultilingualModel ? MULTILINGUAL_VOCAB_FILE : ENGLISH_ONLY_VOCAB_FILE;
+        File vocabFile = new File(sdcardDataFolder, vocabFileName);
+
+        mWhisper = new Whisper(this);
+        mWhisper.loadModel(modelFile, vocabFile, isMultilingualModel);
+        Log.d(TAG, "Initialized: " + modelFile.getName());
+        mWhisper.setListener(new Whisper.WhisperListener() {
+            @Override
+            public void onUpdateReceived(String message) { }
+
+            @Override
+            public void onResultReceived(WhisperResult whisperResult) {
+                if (whisperResult.getResult().trim().length() > 0){
+                    Log.d(TAG, whisperResult.getResult().trim());
+                    try {
+                        callback.endOfSpeech();
+                        Bundle results = new Bundle();
+                        ArrayList<String> resultList = new ArrayList<>();
+                        resultList.add(whisperResult.getResult().trim());
+                        results.putStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION, resultList);
+                        callback.results(results);
+                    } catch (RemoteException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        });
+    }
+
+    private void startRecording() {
+        mRecorder.start();
+        recognitionCancelled = false;
+    }
+
+    private void startTranscription() {
+        if (!recognitionCancelled){
+            mWhisper.setAction(Whisper.ACTION_TRANSCRIBE);
+            mWhisper.start();
+            Log.d(TAG,"Start Transcription");
+        }
+    }
+
+    @Override
+    public void onDestroy (){
+        deinitModel();
+    }
+    private void deinitModel() {
+        if (mWhisper != null) {
+            mWhisper.unloadModel();
+            mWhisper = null;
+        }
+    }
+
+    private void checkRecordPermission(Callback callback) {
+        int permission = ContextCompat.checkSelfPermission(this, android.Manifest.permission.RECORD_AUDIO);
+        if (permission != PackageManager.PERMISSION_GRANTED){
+            Log.d(TAG,getString(R.string.need_record_audio_permission));
+            try {
+                callback.error(ERROR_INSUFFICIENT_PERMISSIONS);
+            } catch (RemoteException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+}
